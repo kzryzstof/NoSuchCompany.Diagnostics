5f297af2c0920c67dae0df31e49513763e9a8e3e
Christophe Commeyne <christophecommeyne@nosuchcompany.ca>
2018-08-11 12:04:14 -0400

    git create branch feature-1

--- src/NoSuchCompany.Diagnostics/Argument.DateTime.cs
+++ src/NoSuchCompany.Diagnostics/Argument.DateTime.cs
@@ -8,10 +8,21 @@ namespace NoSuchCompany.Diagnostics
         /// </summary>
         /// <param name="inst">Inst.</param>
         /// <param name="instName">Inst name.</param>
-        public static void ThrowIfNotUtc(DateTime inst, string instName)
+        public static void ThrowIfIsNotUtc(in DateTime inst, string instName)
         {
             if (inst.Kind != DateTimeKind.Utc)
-                throw new ArgumentException(instName, $"{instName} is not allowed to be null.");
+                throw new ArgumentException(instName, $"{instName} is not expressed in the UTC time-zone.");
+        }
+
+        /// <summary>
+        /// Throws an <see cref="ArgumentException"/> if <paramref name="inst"/> is not specified in a local time-zone.
+        /// </summary>
+        /// <param name="inst">Inst.</param>
+        /// <param name="instName">Inst name.</param>
+        public static void ThrowIfIsNotLocal(in DateTime inst, string instName)
+        {
+            if (inst.Kind != DateTimeKind.Local)
+                throw new ArgumentException(instName, $"{instName} is not expressed in a local time-zone.");
         }
     }
 }
--- src/NoSuchCompany.Diagnostics/Fluent.DateTime.cs
+++ src/NoSuchCompany.Diagnostics/Fluent.DateTime.cs
@@ -11,7 +11,19 @@ namespace NoSuchCompany.Diagnostics
         /// <param name="instName">Inst name.</param>
         public static DateTime ThrowIfIsNotUtc(this DateTime inst, string instName)
         {
-            Argument.ThrowIfNotUtc(inst, instName);
+            Argument.ThrowIfIsNotUtc(inst, instName);
+
+            return inst;
+        }
+
+        /// <summary>
+        /// Throws an <see cref="ArgumentException"/> if <paramref name="inst"/> is not specified in a local time-zone.
+        /// </summary>
+        /// <param name="inst">Inst.</param>
+        /// <param name="instName">Inst name.</param>
+        public static DateTime ThrowIfIsNotLocal(this DateTime inst, string instName)
+        {
+            Argument.ThrowIfIsNotLocal(inst, instName);
 
             return inst;
         }
